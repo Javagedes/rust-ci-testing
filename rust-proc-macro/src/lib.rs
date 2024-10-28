294c4d3e6da0ce1e9c8cf9b1ccbb2cdcb993781e
Joey Vagedes <joey.vagedes@gmail.com>
2024-10-28 08:41:11 -0700

    update

--- rust-proc-macro/src/lib.rs
+++ rust-proc-macro/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn hello(_attr: proc_macro::TokenStream, _item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    todo!()
 }
