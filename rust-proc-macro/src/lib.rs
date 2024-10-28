acf2fe26084028096682daff34bb720849c8174f
Joey Vagedes <joey.vagedes@gmail.com>
2024-10-28 08:49:02 -0700

    update

--- rust-proc-macro/src/lib.rs
+++ rust-proc-macro/src/lib.rs
@@ -1,4 +1,7 @@
 #[proc_macro_attribute]
-pub fn hello(_attr: proc_macro::TokenStream, _item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn hello(
+    _attr: proc_macro::TokenStream,
+    _item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     todo!()
 }
